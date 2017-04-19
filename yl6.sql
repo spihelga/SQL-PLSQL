03fc655bfcaa77dbe1b8fb54a5573707aedfdca9
Sander Pihelgas <sander.pihelgas@itcollege.ee>
2017-04-20 00:09:16 +0300

    yl6

--- yl6.sql
+++ yl6.sql
@@ -1,8 +1,10 @@
 COLUMN nimetus FORMAT A15 NEW_VALUE nimetus NOPRINT
 COLUMN oppejoud FORMAT A15 NEW_VALUE oppejoud NOPRINT
+COLUMN Nimi FORMAT A15
 BTITLE CENTER 'Õppejõud: ' oppejoud
 TTITLE CENTER 'Aine: ' nimetus SKIP 2
 BREAK ON nimetus SKIP PAGE
+SET PAGESIZE 15
 ACCEPT nimetus PROMT 'Sisestage õppeaine nimetus: '
 SELECT
 perenimi || ', ' || eesnimi as "Nimi",
@@ -16,9 +18,9 @@ yliopilased y,
 koduylesanded k,
 oppeained o
 WHERE
-k.yliopisalse_id=y.id AND
+k.yliopilase_id=y.id AND
 k.kursusekood=o.kood AND
-UPPER(nimetus) LIKE UPPER ('%&nimetus%');
+UPPER(nimetus) LIKE UPPER ('%&nimetus%')
+ORDER BY nimetus;
 CLEAR COLUMN
-
 
